41fc8df85f1e632cbee79edbcc9aa6d26ceb263f
iKevinsz <fioreelokevin@gmail.com>
2025-12-09 11:14:12 -0300

    primeiro commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,29 @@
+export function Header() {
+
+return (
+
+<header className="flex px-2 py-4 bg-zinc-900 text-white">
+
+<div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+
+<div>Area do Cliente</div>
+
+<nav>
+
+<ul className="flex items-center justify-center gap-2">
+
+<li>Suporte: DATACAIXA TECNOLOGIA</li>
+
+<li>+55 (11) 99999-9999</li>
+
+</ul>
+
+</nav>
+
+</div>
+
+</header>
+
+);
+
+}
